5a7e5e83bb7121ff7c3075c1dbf3952e410a9955
phaothu2002x <truongthuan21342@gmail.com>
2024-04-04 03:05:57 +0700

    styling sidebar #1

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -66,12 +66,15 @@ const SideBar = () => {
           href={item.path}
           key={item.path}
           className={cn(
-            "group flex h-14 w-14 items-center justify-center rounded-full border-2 transition hover:border-gray-200 hover:bg-[#7743DB]/90",
-            isActive(item.path) ? "bg-[#7743DB]/90" : "",
+            "border-1 group flex h-14 w-full items-center justify-center rounded-lg border-r-2 border-transparent text-gray-100 transition hover:border-sky-400 hover:bg-sky-300/50",
+            isActive(item.path) ? "  border-r-2 bg-sky-300/50  " : "",
           )}
         >
           <item.icon
-            className={cn("h-4 w-4 text-white group-hover:text-white  ")}
+            className={cn(
+              "h-5 w-5  transition group-hover:h-6  group-hover:w-6 group-hover:text-white",
+              isActive(item.path) ? "h-6 w-6" : "",
+            )}
           />
         </Link>
       ))}
